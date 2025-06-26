e0213b320d6d14d55bf9fd94ff2e12a194f36456
Dipesh-rai <rdipesh48@gmil.com>
2025-06-26 15:08:28 +0545

    update

--- pilgrimage/settings.py
+++ pilgrimage/settings.py
@@ -135,6 +135,8 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+# Required for Vercel
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
