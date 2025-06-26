84c6a2a1057f28b4cb73aaed3877c13ca1c78db5
Dipesh-rai <rdipesh48@gmil.com>
2025-06-26 14:31:39 +0545

    new update

--- pilgrimage/settings.py
+++ pilgrimage/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-8!%sqzg6xspytqm8n=jx!8i26mis=0o1e9dk44n0ob4d3avx_a
 SECRET_KEY = os.environ.get('SECRET_KEY', SECRET_KEY) 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app']
 # Application definition
